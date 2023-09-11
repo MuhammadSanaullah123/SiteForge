79bb3ba89bd3ff2c8aa2896368aa109dab1cad13
MuhammadSanaullah123 <muhammed14336@gmail.com>
2023-09-11 16:14:11 +0500

    again

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(cookieParser());
 app.use("/api/github", require("./routes/api/github"));
 app.use("/api/netlify", require("./routes/api/netlify"));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
